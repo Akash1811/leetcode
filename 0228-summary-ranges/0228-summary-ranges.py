d6fd44877dde1934ba057ba48d3ac1fc48c9f309
Akash1811 <67039054+Akash1811@users.noreply.github.com>
2024-08-31 22:34:06 +0530

    Time: 37 ms (39.92%), Space: 16.4 MB (68.21%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -7,9 +7,9 @@ class Solution:
             while i<len(nums)-1 and nums[i]+1==nums[i+1]:
                 i+=1
             if start!=nums[i]:
-                ans.append(str(start)+'->'+ str(nums[i]))
+                ans.append(str(start)+ '->' + str(nums[i]))
             else:
                 ans.append(str(nums[i]))
             i+=1
-        return ans        
+        return ans
         
