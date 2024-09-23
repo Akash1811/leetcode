5e0396e162e2713c66ae1c650f5dee7d4074c4d5
Akash1811 <67039054+Akash1811@users.noreply.github.com>
2024-09-23 23:58:00 +0530

    Time: 35 ms (78.13%), Space: 16.4 MB (87.53%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,11 +1,13 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         n=len(matrix)
-        matrix.reverse()
+        #matrix[::-1]
         for r in range(n):
             for c in range(r):
                 matrix[r][c],matrix[c][r]=matrix[c][r],matrix[r][c]
-                #matrix.reverse()
+        for r in range(n):
+            for c in range(n//2):
+                matrix[r][c],matrix[r][n-c-1]=matrix[r][n-c-1],  matrix[r][c]
         """
         Do not return anything, modify matrix in-place instead.
         """
