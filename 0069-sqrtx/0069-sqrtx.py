8a9ad49e56ee93489b28c47d9053817913abac01
Akash1811 <67039054+Akash1811@users.noreply.github.com>
2024-11-30 00:07:56 +0530

    Time: 3 ms (62.59%), Space: 17.1 MB (10.25%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,4 +1,14 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**(1/2))
+        l,r=1,x
+        while l<=r:
+            m=(l+r)//2
+            msqre=m*m
+            if msqre==x:
+                return m
+            elif msqre<x:
+                l=m+1
+            else:
+                r=m-1
+        return r
         
